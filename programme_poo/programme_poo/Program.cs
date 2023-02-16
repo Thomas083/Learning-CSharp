044fc753525ce2568a5363c10187dfbfd5b15ad8
Debray Thomas <thomasdebray83@gmail.com>
2023-02-16 09:29:50 +0100

    Faiblesse de la programmation impérative
    étudiant n'est pas vraiment un emploi il faut faire attention que les index des liste soit les même  si on rajoute des enfants on pourrait imaginer vouloir savoir qui est le professeur etc et c'est compliqué avec la programmation impérative

---
+++ programme_poo/programme_poo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace programme_poo
+{
+    class Personne
+    {
+        string nom;
+        int age;
+        string emploi;
+    }
+    internal class Program
+    {
+        static void ShowInfoPeople(string nom, int age, string emploi)
+        {
+            Console.WriteLine("Nom : " + nom);
+            Console.WriteLine(" AGE : " + age + " ans");
+            Console.WriteLine("EMPLOI : " + emploi);
+        }
+        static void Main(string[] args)
+        {
+            var noms = new List<string> { "Pierre", "Paul", "Jacque"};
+            var ages = new List<int> { 30, 35, 20};
+            var emplois = new List<string> { "Développeur", "Professeur", "Etudiant"};
+
+            for(int i = 0; i < noms.Count; i++)
+            {
+                ShowInfoPeople(noms[i], ages[i], emplois[i]);
+            }
+        }
+    }
+}
