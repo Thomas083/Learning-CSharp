fef2e3634a4409e2b9ebd056af67b40e15e7574f
Debray Thomas <thomasdebray83@gmail.com>
2023-02-22 17:14:27 +0100

    gestion des fichier

---
+++ programme_fichier/programme_fichier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace programme_fichier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
