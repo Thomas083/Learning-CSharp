b49a37e96458f3a15dd6b4e5d4a6bcce6c91b635
Debray Thomas <thomasdebray83@gmail.com>
2023-02-23 09:34:44 +0100

    Gestion d'exception

--- programme_fichier/programme_fichier/Program.cs
+++ programme_fichier/programme_fichier/Program.cs
@@ -5,8 +5,19 @@
         static void Main(string[] args)
         {
             //File.WriteAllText("monFichier.txt", "Voici un super contenue pour mon super fichier texte");
-            string resultat = File.ReadAllText("monFichier.txt");
-            Console.WriteLine(resultat);
+            try
+            {
+                string resultat = File.ReadAllText("monFichiers.txt");
+                Console.WriteLine(resultat);
+            } 
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("ERREUR : ce fichier n'existe pas \n" + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERREUR : " + ex.Message);
+            }
         }
     }
 }
