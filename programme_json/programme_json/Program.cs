97f9faeb924b17d843dcc7477472cf8d128deb22
Debray Thomas <thomasdebray83@gmail.com>
2023-02-23 15:44:06 +0100

    Programme JSON

---
+++ programme_json/programme_json/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace programme_json
+{
+
+    class Personne
+    {
+        public string nom;
+        public int age;
+
+        public void Afficher ()
+        {
+            Console.WriteLine("Nom: " + nom + " - age: " + age + " ans");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne person = new Personne();
+            person.nom = "Thomas";
+            person.age = 25;
+            person.Afficher();
+
+            string json = JsonConvert.SerializeObject(person);
+            Console.WriteLine(json);
+        }
+    }
+}
