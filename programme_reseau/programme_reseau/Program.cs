4399bdef5cb6f0dd13e544fb65d7ea71a91c6942
Debray Thomas <thomasdebray83@gmail.com>
2023-02-24 10:48:21 +0100

    Code d'erreur
    gestion des erreur et récupération du code d'erreur

--- programme_reseau/programme_reseau/Program.cs
+++ programme_reseau/programme_reseau/Program.cs
@@ -6,13 +6,20 @@ namespace programme_reseau
     {
         static void Main(string[] args)
         {
-            string url = "https://codeavecjonathan.com/res/exemple.html";
+            string url = "https://codeavecjonathan.com/res/exempsle.html";
 
             var webClient = new WebClient();
+            try
+            {
+                string reponse = webClient.DownloadString(url);
+                Console.WriteLine(reponse);
+            }
+            catch (WebException ex)
+            {
+                var statusCode = ((HttpWebResponse)ex.Response).StatusCode;
+                Console.WriteLine("ERREUR RESEAU : " + statusCode);
+            }
 
-            string reponse = webClient.DownloadString(url);
-
-            Console.WriteLine(reponse);
         }
     }
 }
