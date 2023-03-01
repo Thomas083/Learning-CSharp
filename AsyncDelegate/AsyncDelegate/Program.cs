2f73aa199521daf8c9b3021591896b7b086bb153
Debray Thomas <thomasdebray83@gmail.com>
2023-03-01 14:59:08 +0100

    Async delegate

---
+++ AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+       static bool downloading = false;
+        static void Main(string[] args)
+        {
+            Console.Write("Téléchargement...");
+            var webClient = new WebClient();
+            string url = "https://codeavecjonathan.com/res/bateau.jpg";
+            //webClient.DownloadFile(url, "bateau.jpg");
+            downloading = true;
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+            webClient.DownloadFileAsync(new Uri(url), "bateau.jpg");
+            while(downloading)
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Fin du programme");
+        }
+
+        private static void WebClient_DownloadFileCompleted(object? sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Téléchargement terminé");
+            downloading = false;
+        }
+    }
+}
