5ca94025b555261e62a43b4837bd12592207d7f2
Debray Thomas <thomasdebray83@gmail.com>
2023-03-01 15:51:50 +0100

    init-only properties

--- News_cs9/News_cs9/Program.cs
+++ News_cs9/News_cs9/Program.cs
@@ -1,7 +1,28 @@
-﻿
-        Console.WriteLine("Hello, World!");
-        foreach(var arg in args)
+﻿namespace News_cs9
+{
+    class Personne
+    {
+        public string nom { get; init; }
+        public int age { get; init; }
+
+        public void Show()
         {
-            Console.WriteLine(arg);
+            Console.WriteLine("Nom : " + nom + " Age : " + age + "ans");
         }
-        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstPerson = new Personne { nom ="Thomas", age = 25 };
+            var secondPerson = new Personne { nom ="Maxence", age = 22 };
+
+            firstPerson.Show();
+            Console.WriteLine("Hello, World!");
+            foreach(var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
