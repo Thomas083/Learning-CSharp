e66052eb3734ddf08c890ef6d5426b92f9fb3136
Debray Thomas <thomasdebray83@gmail.com>
2023-03-01 11:54:17 +0100

    DateTime
    Using format / CultureInfo

---
+++ ProgrammeDateTime/ProgrammeDateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ProgrammeDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            CultureInfo culture = CultureInfo.GetCultureInfo("fr-FR");
+
+            Console.WriteLine(date.ToString("dddd dd MMMM yyyy", culture));
+
+            DateTime dateDemain = date.AddDays(1);
+            Console.WriteLine("Demain on sera le : " + dateDemain.ToString("dddd dd MMMM yyyy", culture));
+
+            var diff = dateDemain - date;
+            
+            Console.WriteLine("Différence jours : " + diff.TotalDays);
+        }
+
+    }
+}
