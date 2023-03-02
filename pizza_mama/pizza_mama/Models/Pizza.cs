4ef4534d7b4906bbd4e74c8ebcdb2a53119089e0
Debray Thomas <thomasdebray83@gmail.com>
2023-03-02 16:03:53 +0100

    DataContext

--- pizza_mama/pizza_mama/Models/Pizza.cs
+++ pizza_mama/pizza_mama/Models/Pizza.cs
@@ -2,5 +2,10 @@
 {
     public class Pizza
     {
+        public int PizzaID { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+        public bool vegetarian { get; set; }
+        public string ingredient { get; set; }
     }
 }
