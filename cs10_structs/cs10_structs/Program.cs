a239128281dab3c8274832c6d4ef08c1a7ba4842
Debray Thomas <thomasdebray83@gmail.com>
2023-03-02 10:27:14 +0100

    CS10 struct and record
    On peut surcharger le constructeur par defaut d'une struct
    Ajout des record struct (valeur) et record class (référence)
    readonly record struct permet d'avoir des valeurs immutable

---
+++ cs10_structs/cs10_structs/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace cs10_structs
+{
+    struct PersonneStruct
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+
+        public PersonneStruct()
+        {
+            name = "Inconnue";
+            age = -1;
+        }
+    }
+    record struct PersonneRecord(string name, int age);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ps1 = new PersonneStruct();
+
+            var pr1 = new PersonneRecord("Paul", 20);
+            var pr2 = new PersonneRecord("Paul", 20);
+
+
+            Console.WriteLine(pr1);
+            Console.WriteLine(pr2);
+            Console.WriteLine(pr1 == pr2);
+        }
+    }
+}
