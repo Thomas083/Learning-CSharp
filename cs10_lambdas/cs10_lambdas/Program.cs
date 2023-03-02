aeeb961ea1dafb9e97036402b03f3043c2398e03
Debray Thomas <thomasdebray83@gmail.com>
2023-03-02 10:48:22 +0100

    CallerArgumentExpression

--- cs10_lambdas/cs10_lambdas/Program.cs
+++ cs10_lambdas/cs10_lambdas/Program.cs
@@ -1,13 +1,16 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 int Filter1(int v)
 {
     if(v<0) v = 0; return v;
 }
 
-int Operation(int a, int b, Func<int, int> f)
+int Operation(int a, int b, Func<int, int> f, [CallerArgumentExpression("f")]string? paramName = null)
 { 
     a=f(a);
     b=f(b);
+    Console.WriteLine("Fonction de filtrage : " + paramName);
     return a + b; 
 }
 
