2425fd4f4f6e8cd5c1203653e7fa34bf041419e7
Debray Thomas <thomasdebray83@gmail.com>
2023-03-03 11:05:29 +0100

    Some traduction

--- pizza_mama/pizza_mama/Models/Pizza.cs
+++ pizza_mama/pizza_mama/Models/Pizza.cs
@@ -1,11 +1,17 @@
-﻿namespace pizza_mama.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_mama.Models
 {
     public class Pizza
     {
         public int PizzaID { get; set; }
+        [Display(Name = "Nom")]
         public string name { get; set; }
+        [Display(Name = "Prix")]
         public float price { get; set; }
+        [Display(Name = "Végétarienne")]
         public bool vegetarian { get; set; }
+        [Display(Name = "Ingrédient")]
         public string ingredient { get; set; }
     }
 }
