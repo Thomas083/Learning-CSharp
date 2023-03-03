1ae188b7869c6a0bc7d47436e908c3598b95f719
Debray Thomas <thomasdebray83@gmail.com>
2023-03-03 12:42:11 +0100

    Login with login/password in appsettings.json
    Alert if combination of login/password invalid

--- pizza_mama/pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace pizza_mama.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        public bool DisplayInvalidAccountMessage = false;
+        IConfiguration configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public IActionResult OnGet()
         {
             if(HttpContext.User.Identity.IsAuthenticated)
@@ -19,18 +26,24 @@ namespace pizza_mama.Pages.Admin
             return Page();
         }
         public async Task<IActionResult> OnPost(string username, string password, string ReturnUrl) 
-        { 
-            if (username == "admin")
+        {
+            var authSection = configuration.GetSection("Auth");
+
+            string adminLogin = authSection["AdminLogin"];
+            string adminPassword = authSection["AdminPassword"];
+            if (username == adminLogin && password == adminPassword)
             {
+				DisplayInvalidAccountMessage = false;
                 var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name,username),
                 };
                 var claimsIdentity = new ClaimsIdentity(claims, "Login"); 
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                return Redirect(ReturnUrl == null ? "/Admin/pizzas" : ReturnUrl);
+				return Redirect(ReturnUrl == null ? "/Admin/pizzas" : ReturnUrl);
             }
-            return Page();
+			DisplayInvalidAccountMessage = true;
+			return Page();
         }
 
         public async Task<IActionResult> OnGetLogout()
