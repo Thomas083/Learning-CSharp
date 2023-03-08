0fa45311918a058777ee37c2c65091d85e6e53f3
Debray Thomas <thomasdebray83@gmail.com>
2023-03-08 15:10:26 +0100

    Starting vue for pizza web project
    show pizza
    add price
    adding css
    adding images
    adapt the background image and display
    adding logo and href to home page
    style for h1

---
+++ pizza_mama/pizza_mama/Pages/MenuPizzas.cshtml.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_mama.Models;
+
+namespace pizza_mama.Pages
+{
+    public class MenuPizzasModel : PageModel
+    {
+        private readonly pizza_mama.Data.DataContext _context;
+
+        public MenuPizzasModel (pizza_mama.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Pizza> Pizza { get; set; }
+        public async Task OnGetAsync()
+        {
+            Pizza = await _context.Pizzas.ToListAsync();
+        }
+    }
+}
